1d706c6e8e6e29c46c6007d1591549d509bacf24
LeMinhThienAn <leminhthinan1601@gmail.com>
2019-10-23 21:05:52 +0700

    Game

---
+++ Game/Game/Controller.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    class Controller
+    {
+        private Board Game;
+        private Player P1, P2;
+
+        public Controller()
+        {
+            this.Game = new Board(3, 4, 6);
+            this.P1 = new Player("Thien An");
+            this.P2 = new Player("Phuong Linh");
+        }
+
+        public void PlayGame()
+        {
+            Game.PrintGame();
+            while (true)
+            {
+                Console.WriteLine(P1.GetName() + " play");
+                P1.PickBall(Game);
+                Game.PrintGame();
+                if (Game.IsGameOver())
+                {
+                    Console.WriteLine("P1 game over");
+                    break;
+                }
+                Console.WriteLine(P2.GetName() + " play");
+                P2.PickBall(Game);
+                Game.PrintGame();
+                if (Game.IsGameOver())
+                {
+                    Console.WriteLine("P2 game over");
+                    break;
+                }
+            }
+        }
+    }
+}
